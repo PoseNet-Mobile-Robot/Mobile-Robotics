44843f613c8cbfefeb7c41e472da03fdffb56bf2
zeroAska <trotwoodray@gmail.com>
2018-04-15 23:20:23 -0400

    ready to merge

--- gtsam_main.py
+++ gtsam_main.py
@@ -5,7 +5,7 @@
 
 from readOdometry import odometry
 from readImage import readImage
-import gen_data_nclt_new
+import gen_data_cam
 from gtsamSolver import PoseNetiSam
 from train import trainer
 import matplotlib.pyplot as plt
@@ -30,7 +30,7 @@ def main(pathOdom_,pathImage_,pathWeight_):
     iSam = PoseNetiSam()
     
     # initilize posenet
-    poseNet = trainer(pathWeight,pathImage,100,False,False,True) # comment for debug
+    poseNet = trainer(pathWeight,pathImage,100,False,True,True) # comment for debug
 
     # initialize the plot tool
     fig = plt.figure(1)
@@ -43,7 +43,7 @@ def main(pathOdom_,pathImage_,pathWeight_):
     
     # read data
     images = readImage(pathImage)
-    datasource = gen_data_nclt_new.get_data()
+    datasource = gen_data_cam.get_data()
     startTimestamp = images.getStartId() # start timestamp
     endTimestamp = images.getEndId() # end timestamp
     odom = odometry(pathOdom,startTimestamp,endTimestamp)
@@ -57,7 +57,7 @@ def main(pathOdom_,pathImage_,pathWeight_):
     evalTime = list()
     
     # prior info
-    priorMu = [32,-170,-0.3]
+    priorMu = [-56,-79,-1.56]
     priorCov = [10,10,10]
     iSam.initialize(priorMu,priorCov) # adding prior
     _,imgTimestamp,currGT = images.getImage(startId)
@@ -80,7 +80,7 @@ def main(pathOdom_,pathImage_,pathWeight_):
     groundTruth.append(currGT)
 
     iterations = images.length()-1
-    # iterations = 50
+    # iterations = 50 # comment for release
     # localization begins here
     print("\nBegin localization:\n")
     for i in tqdm(range(iterations)):
@@ -129,24 +129,35 @@ def main(pathOdom_,pathImage_,pathWeight_):
         evalTime.append(time.time()-tic)
         # currentEst = currGT # comment for release
         records.append(currentEst)
+
         
         # increment the Id
         startId += 1
 
         # plot tool for the calculated trajetory
-        x = records[-1][0]
-        y = records[-1][1]
+        x = records[-1][1]
+        y = records[-1][0]
         plt.scatter(x,y,c='g') # plot line from x_t-1 to x_t
-        x = groundTruth[-1][0]
-        y = groundTruth[-1][1]
+        x = groundTruth[-1][1]
+        y = groundTruth[-1][0]
         plt.scatter(x,y,c='r') # plot line of ground truth from x_t-1 to x_t
-        plt.scatter(measurement[0],measurement[1],c='b') # plot points of posenet prediction
+        plt.scatter(measurement[1],measurement[0],c='b') # plot points of posenet prediction
+
         if(i == 0):
             plt.legend(['prediction',' ground truth','posenet prediction'])
 
         # plt.draw() # comment if not dynamic plotting
         # plt.pause(0.01) # whether need to pause?
         # print("Done {} iterations, {} iterations in total".format(i+1,iterations))
+
+    # print mean and stddev error
+    error = np.zeros([images.length(),3])
+    for i in range(images.length()):
+        error[i,:] = abs(iSam.getEstimate(i+1)-np.array(groundTruth[i]))
+    meanErr = np.sum(error,axis=0)/len(error)
+    stdErr = np.std(error,axis=0)
+    print("The mean error is {} and standard deviation is {}.".format(meanErr,stdErr))
+
         
     # store estimation
     pickleOut = open('dict.{0}_estimation'.format(resultsId),'wb')
@@ -169,14 +180,14 @@ def main(pathOdom_,pathImage_,pathWeight_):
 if __name__=='__main__':
 
     # use gpu 0
-    # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    #os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
     # reading argument
     argv = sys.argv
     if len(argv) < 3:
         argv = list()
-        argv.append('/home/eecs568/Documents') # path to odometry
-        argv.append('/home/eecs568/eecs568/Mobile-Robotics/cam4_test/') # path to images
-        argv.append('/home/eecs568/eecs568/Mobile-Robotics/success_models/nclt_new/20180409-130922model_epoch_4.ckpt') # path to weights
+        argv.append('./nclt_03_31/odometry') # path to odometry
+        argv.append('./nclt_03_31/test/') # path to images
+        argv.append('/home/eecs568/eecs568/new_version/Mobile-Robotics/20180415-201632model_epoch_4.ckpt') # path to weights
     
     main(argv[0],argv[1],argv[2])
